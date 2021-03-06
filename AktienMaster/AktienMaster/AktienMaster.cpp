c6a5d3c9dfbd7a68caf77430890c040b7783a5d3
jacob-sch <jacob.schmauss@gmail.com>
2021-03-06 16:41:48 +0100

    Update AktienMaster.cpp

--- AktienMaster/AktienMaster/AktienMaster.cpp
+++ AktienMaster/AktienMaster/AktienMaster.cpp
@@ -1,10 +1,14 @@
+
+
 #include <iostream>
 
-
-int main() {
-	std::cout << "Hello World!\n";
+int main()
+{
+    std::cout << "Hello World!\n";
 	std::cout << "Test Changes\n";
-	std::cout << "New Changes" << std::endl;
+    std::cout << "New Changes" << std::endl;
+    std::cout << "Test VSCode" << std::endl;
 
-	return 0;
+    return 0;
+
 }
