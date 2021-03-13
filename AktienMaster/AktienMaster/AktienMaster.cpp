1be52b03655be57887fec0e4464869de78d2930c
Dario Bruckner <dariobruckner@gmail.com>
2021-03-13 15:44:05 +0100

    First hashFunction implemented
    First iteration of the hashfunction implemented

--- AktienMaster/AktienMaster/AktienMaster.cpp
+++ AktienMaster/AktienMaster/AktienMaster.cpp
@@ -1,14 +1,38 @@
-
-
 #include <iostream>
+#include <stdio.h>
+#include <string>
+#include <string.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-	std::cout << "Test Changes\n";
-    std::cout << "New Changes" << std::endl;
-    std::cout << "Test VSCode" << std::endl;
+#include "aktie.h";
 
-    return 0;
+#define PRIME 1009;
+
+
+aktie* hashtable[1008];
+
+
+
+int power(int base, int exponent) {
+    if (exponent == 0) {
+        return 1;
+    }
+    return base * power(base, exponent - 1);
+}
+
+int hashFunc(std::string str, int n) {
+    int sum = 0;
+
+    for (int i = n - 1; i > 0; i--) {
+        sum = (sum + str[n - i - 1] * power(31, i)) % PRIME;
+    }
+
+    return sum;
+}
+
+
+int main() {
+    std::string key = "AAPL";
+
+    std::cout << hashFunc(key, key.length());
 
 }
