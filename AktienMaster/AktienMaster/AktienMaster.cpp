f99cc03e3de578f636d1f3f35d96452a722db836
Dario Bruckner <dariobruckner@gmail.com>
2021-03-13 20:18:58 +0100

    Added "Quadratische Sondierung" to hashfunction
    added "quadratische Sondierung" to hashfunction, now is able to store all 1009 potential elements in array (changed to String array for testing purposes)

--- AktienMaster/AktienMaster/AktienMaster.cpp
+++ AktienMaster/AktienMaster/AktienMaster.cpp
@@ -2,13 +2,12 @@
 #include <stdio.h>
 #include <string>
 #include <string.h>
-
-#include "aktie.h";
+#include <time.h>
 
 #define PRIME 1009;
 
 
-aktie* hashtable[1008];
+std::string hashtable[1009];
 
 
 
@@ -20,6 +19,7 @@ int power(int base, int exponent) {
 }
 
 int hashFunc(std::string str, int n) {
+    
     int sum = 0;
 
     for (int i = n - 1; i > 0; i--) {
@@ -29,10 +29,56 @@ int hashFunc(std::string str, int n) {
     return sum;
 }
 
+void fillhashtable(int arr) {
+    std::string word = "";
+
+
+    for (int i = 0; i < 1009; i++) {
+        hashtable[i] = "no";
+    }
+
+
+    for (int i = 0; i < arr; i++) {
+        for (int j = 0; j < 4; j++) {
+            int random = rand() % 26 + 97;
+            char letter = char(random);
+            word.push_back(letter);
+        }
+
+        int pos = hashFunc(word, word.length());
+        if (hashtable[pos] != "no") {
+            int counter = 1;
+            while (1) {
+                pos += (counter * counter);
+                pos %= PRIME;
+                
+                if (hashtable[pos] == "no") {
+                    break;
+                }
+                counter++;
+            }
+        }
+        hashtable[pos] = word;
+        word = "";
+    }
+}
+
+
 
 int main() {
-    std::string key = "AAPL";
+    time_t t;
+    srand((unsigned)time(&t));
+    
+    fillhashtable(1009);
+    int counter = 0;
+    for (int i = 0; i < 1009; i++) {
+        if (hashtable[i] != "no") {
+            counter++;
+            std::cout << counter << ".) Slot " << i << " is " << hashtable[i] << std::endl;
+        }
+    }
 
-    std::cout << hashFunc(key, key.length());
+
+    //std::cout << hashFunc(key, key.length());
 
 }
