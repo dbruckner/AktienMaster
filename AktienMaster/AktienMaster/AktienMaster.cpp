a00130bc89d24a4d35adf9528ad195319afeae2d
Dario Bruckner <dariobruckner@gmail.com>
2021-03-21 18:11:23 +0100

    Load database func added
    added database load function

--- AktienMaster/AktienMaster/AktienMaster.cpp
+++ AktienMaster/AktienMaster/AktienMaster.cpp
@@ -18,37 +18,37 @@ Aktie* hashtableAbb[1009];
 
 
 
-int power(int base, int exponent) {
-	if (exponent == 0) {
+int power(int base, int exponent){
+	if(exponent == 0){
 		return 1;
 	}
 	return base * power(base, exponent - 1);
 }
 
-int hashFunc(std::string str, int n) {
+int hashFunc(std::string str, int n){
 
 	int sum = 0;
 
-	for (int i = n - 1; i > 0; i--) {
+	for(int i = n - 1; i > 0; i--){
 		sum = (sum + str[n - i - 1] * power(31, i)) % PRIME;
 	}
 
 	return sum;
 }
 
-int calculateHashPosition(std::string name, Aktie* hashtable[]) {
+int calculateHashPosition(std::string name, Aktie* hashtable[]){
 	int pos = 0;
 	pos = hashFunc(name, name.length());
 
 
-	if (hashtable[pos] != nullptr) {
+	if(hashtable[pos] != nullptr){
 		int counter = 1;
-		while (1) {
+		while(1){
 			pos += (counter * counter);
 			pos %= PRIME;
 
-			if (hashtable[pos] == nullptr || hashtable[pos]->getName() == "deleted") {
-				if (hashtable[pos] != nullptr) {
+			if(hashtable[pos] == nullptr || hashtable[pos]->getName() == "deleted"){
+				if(hashtable[pos] != nullptr){
 					hashtable[pos]->~Aktie();
 					hashtable[pos] = nullptr;
 				}
@@ -62,29 +62,29 @@ int calculateHashPosition(std::string name, Aktie* hashtable[]) {
 	return pos;
 }
 
-void insertIntoHashtables(std::string name, std::string WKN, std::string abb) {
+Aktie* insertIntoHashtables(std::string name, std::string WKN, std::string abb){
 	Aktie* aktie = new Aktie(name, WKN, abb);
 
 	hashtableNames[calculateHashPosition(name, hashtableNames)] = aktie;
 	hashtableAbb[calculateHashPosition(abb, hashtableAbb)] = aktie;
-
+	return aktie;
 }
 
 
-int searchPos(std::string name, Aktie* hashtable[], int type) {
+int searchPos(std::string name, Aktie* hashtable[], int type){
 	std::string word = name;
 	int pos = hashFunc(word, word.length());
 	int counter = 1;
 
-	while (1) {
-		if (type == 2) {
-			if (hashtable[pos] != nullptr && hashtable[pos]->getAbby() == word) {
+	while(1){
+		if(type == 2){
+			if(hashtable[pos] != nullptr && hashtable[pos]->getAbby() == word){
 				return pos;
 			}
-		} else if (counter == 750) {
+		} else if(counter == 750){
 			return -1;
-		} else {
-			if (hashtable[pos] != nullptr && hashtable[pos]->getName() == word) {
+		} else{
+			if(hashtable[pos] != nullptr && hashtable[pos]->getName() == word){
 				return pos;
 			}
 		}
@@ -95,13 +95,13 @@ int searchPos(std::string name, Aktie* hashtable[], int type) {
 	}
 }
 
-void deleteAktie(std::string name) {
+void deleteAktie(std::string name){
 	Aktie* delaktie = new Aktie();
 
 	int posname = searchPos(name, hashtableNames, 1);
-	if (posname == -1) {
+	if(posname == -1){
 		std::cout << "Aktie wurde nicht gefunden" << std::endl;
-	} else {
+	} else{
 		std::string abb = hashtableNames[posname]->getAbby();
 		int posabby = searchPos(abb, hashtableAbb, 2);
 
@@ -112,54 +112,54 @@ void deleteAktie(std::string name) {
 }
 
 
-int searchAktie(int type) {
+int searchAktie(int type){
 	std::string userinput = " ";
 
 	std::cout << "Geben Sie die Information der Aktie ein die Sie suchen: ";
 	std::cin >> userinput;
-	if (type == 2) {
+	if(type == 2){
 
 		return searchPos(userinput, hashtableAbb, type);
-	} else {
+	} else{
 		return searchPos(userinput, hashtableNames, type);
 	}
 }
 
 
-void addAktie() {
+void addAktie(){
 	std::string name = " ";
 	int WKN = 0;
 	std::string abb = " ";
 
 	std::cout << "Geben Sie bitte den Namen der Firma an: ";
 	std::cin >> name;
-	while (1) {
-		if (!cin) {
+	while(1){
+		if(!cin){
 			std::cout << "Bitte geben Sie nur Buchstaben an: ";
 			std::cin >> name;
-		} 		else {
+		} else{
 			break;
 		}
 	}
 
 	std::cout << "Geben Sie bitte nun die WKN an:";
 	std::cin >> WKN;
-	while (1) {
-		if (!cin) {
+	while(1){
+		if(!cin){
 			std::cout << "Bitte geben Sie nur Zahlen an: ";
 			std::cin >> WKN;
-		} 		else {
+		} else{
 			break;
 		}
 	}
 
 	std::cout << "Geben Sie nun bitte zuletzt die abb der von Ihnen ausgew�hlten Firma an: ";
 	std::cin >> abb;
-	while (1) {
-		if (!cin) {
+	while(1){
+		if(!cin){
 			std::cout << "Bitte geben Sie nur Buchstaben an: ";
 			std::cin >> abb;
-		} 		else {
+		} else{
 			break;
 		}
 	}
@@ -167,64 +167,63 @@ void addAktie() {
 	insertIntoHashtables(name, to_string(WKN), abb);
 }
 
-int getNumLines(string filename) {
+int getNumLines(string filename){
 
-    int numLines = 0;
-    string line;
+	int numLines = 0;
+	string line;
 
-    ifstream input(filename);
+	ifstream input(filename);
 
-    while(getline(input, line)) {
-        numLines++;
-    }
+	while(getline(input, line)){
+		numLines++;
+	}
 
-    return numLines;
+	return numLines;
 }
 
-void importData(Aktie &inputAktie) {
+void importData(Aktie& inputAktie){
 
-    string filename = "CSV/"+ inputAktie.getAbby() + ".csv";
+	string filename = "CSV/" + inputAktie.getAbby() + ".csv";
 
-    ifstream input(filename);
+	ifstream input(filename);
 
-    int numLines = getNumLines(filename);
+	int numLines = getNumLines(filename);
 
-    string uselessLine;
-    string nextLine;
-    string nextValue;
-    double Value;
+	string uselessLine;
+	string nextLine;
+	string nextValue;
+	double Value;
 
-    for(int i= 0; i<numLines-30; i++) {
-        getline(input, uselessLine);
-    }
+	for(int i = 0; i < numLines - 30; i++){
+		getline(input, uselessLine);
+	}
 
-    while(getline(input, nextLine)) {
+	while(getline(input, nextLine)){
 
-    int i = 0;
+		int i = 0;
 
-        stringstream s(nextLine);
-        while(getline(s, nextValue, ',')) {
+		stringstream s(nextLine);
+		while(getline(s, nextValue, ',')){
 
-        int n = (i%7);
+			int n = (i % 7);
 
-        if (n!=0) {
-            Value = stod(nextValue);
-            inputAktie.addValue(n, Value);
-        }
-        else {
-            inputAktie.addDate(nextValue);
-        }
+			if(n != 0){
+				Value = stod(nextValue);
+				inputAktie.addValue(n, Value);
+			}         else{
+				inputAktie.addDate(nextValue);
+			}
 
-        i++;
-        }
+			i++;
+		}
 
-    }
+	}
 
 
 
 }
 
-void plotCurve(Aktie& inputAktie) {
+void plotCurve(Aktie& inputAktie){
 
 
 
@@ -237,7 +236,7 @@ void plotCurve(Aktie& inputAktie) {
 	int len = data.size();
 
 
-	for (int i = len - 30; i < len; i++) {
+	for(int i = len - 30; i < len; i++){
 		x.push_back(len - i);
 		y.push_back(data.at(i));
 
@@ -258,57 +257,106 @@ void plotCurve(Aktie& inputAktie) {
 
 }
 
-void saveToFile() {
+void loadDatabase(){
+	string filename = "CSV/database.txt";
 
-    ofstream myfile;
-    myfile.open ("meineAktien.txt");
+	ifstream input(filename);
 
-    for (int i = 0; i < 1009; i++) {
-		if(hashtableNames[i] != nullptr) {
-         if(hashtableNames[i]->getName() != "deleted") {
+	string header;
+
+	string name;
+	string wkn;
+	string abb;
 
 
-            string first = hashtableNames[i]->getName() + "," + hashtableNames[i]->getWKN() + "," + hashtableNames[i]->getAbby() + "\n";
+	string nextLine;
+	string nextValue;
+	double Value;
+	while(1){
 
-            vector<string> dateValues = hashtableNames[i]->getDate();
-            vector<double> openValues = hashtableNames[i]->getVector(open);
-            vector<double> highValues = hashtableNames[i]->getVector(high);
-            vector<double> lowValues = hashtableNames[i]->getVector(low);
-            vector<double> closeValues = hashtableNames[i]->getVector(close);
-            vector<double> adjCloseValues = hashtableNames[i]->getVector(adjClose);
-            vector<double> volumeValues = hashtableNames[i]->getVector(volume);
+		if(std::getline(input, header)){
+			stringstream headerstream(header);
 
-            myfile<<first;
+			std::getline(headerstream, name, ',');
+			std::getline(headerstream, wkn, ',');
+			std::getline(headerstream, abb, ',');
 
-            int len = dateValues.size();
+			Aktie* aktie = insertIntoHashtables(name, wkn, abb);
 
-            for(int j = 0; j < len; j++) {
-                string dataLine = dateValues.at(j) + "," +
-                        to_string(openValues.at(j)) + "," +
-                        to_string(highValues.at(j)) + "," +
-                        to_string(lowValues.at(j)) + "," +
-                        to_string(closeValues.at(j)) + "," +
-                        to_string(adjCloseValues.at(j)) + "," +
-                        to_string(volumeValues.at(j)) + "\n";
 
-                myfile<<dataLine;
-            }
+			int c = 0;
 
-            }
+			for(int i = 0; i < 30; i++){
+
+				std::getline(input, nextLine);
+				stringstream s(nextLine);
+				while(std::getline(s, nextValue, ',')){
+
+					int n = (c % 7);
+
+					if(n != 0){
+						Value = stod(nextValue);
+						aktie->addValue(n, Value);
+					} else{
+						aktie->addDate(nextValue);
+					}
+					c++;
+				}
+			}
+		} else{
+			break;
+		}
+	}
+}
+
+
+void saveToFile(){
+
+	ofstream myfile;
+	myfile.open("database.txt");
+
+	for(int i = 0; i < 1009; i++){
+		if(hashtableNames[i] != nullptr){
+			if(hashtableNames[i]->getName() != "deleted"){
+
+
+				string first = hashtableNames[i]->getName() + "," + hashtableNames[i]->getWKN() + "," + hashtableNames[i]->getAbby() + "\n";
+
+				vector<string> dateValues = hashtableNames[i]->getDate();
+				vector<double> openValues = hashtableNames[i]->getVector(open);
+				vector<double> highValues = hashtableNames[i]->getVector(high);
+				vector<double> lowValues = hashtableNames[i]->getVector(low);
+				vector<double> closeValues = hashtableNames[i]->getVector(close);
+				vector<double> adjCloseValues = hashtableNames[i]->getVector(adjClose);
+				vector<double> volumeValues = hashtableNames[i]->getVector(volume);
+
+				myfile << first;
+
+				int len = dateValues.size();
+
+				for(int j = 0; j < len; j++){
+					string dataLine = dateValues.at(j) + "," +
+						to_string(openValues.at(j)) + "," +
+						to_string(highValues.at(j)) + "," +
+						to_string(lowValues.at(j)) + "," +
+						to_string(closeValues.at(j)) + "," +
+						to_string(adjCloseValues.at(j)) + "," +
+						to_string(volumeValues.at(j)) + "\n";
+
+					myfile << dataLine;
+				}
+
+			}
 
 		}
 	}
 	myfile.close();
-
-
-
-
 }
 
 
-int main() {
+int main(){
 
-	for (int i = 0; i < 1009; i++) {
+	for(int i = 0; i < 1009; i++){
 		hashtableNames[i] = nullptr;
 		hashtableAbb[i] = nullptr;
 	}
@@ -316,7 +364,7 @@ int main() {
 	std::cout << "Willkommen im Aktienmaster Ihr persoehnliches Aktien Tool!" << std::endl;
 	int userinput;
 
-	while (1) {
+	while(1){
 		cout << "Was wollen sie tun?" << endl
 			<< "(1) neue Aktien hinzufuegen" << endl
 			<< "(2) einen Index suchen" << endl
@@ -324,35 +372,36 @@ int main() {
 			<< "(4) Kurve anzeigen" << endl
 			<< "(5) Aktie loeschen" << endl
 			<< "(6) Daten speichern" << endl
-			<< "(7) Daten laden" << endl;
+			<< "(7) Daten laden" << endl
+			<< "(0) Progam beenden";
 		std::cin >> userinput;
-		if (userinput == 1) {
+		if(userinput == 1){
 			addAktie();
-		}else if (userinput == 2) {
+		} else if(userinput == 2){
 			std::cout << "Wollen Sie nach dem Namen oder nach der Abb suchen? (1) nach dem Namen (2) nach der Abb" << std::endl;
 			int type = 0;
 			int pos = 0;
 			std::cin >> type;
-			if (type == 2) {
+			if(type == 2){
 				pos = searchAktie(type);
-			} else {
+			} else{
 				pos = searchAktie(type);
 			}
-			if (pos != -1 && type == 1) {
+			if(pos != -1 && type == 1){
 				hashtableNames[pos]->printAktie();
-			} else if (pos != -1 && type == 2) {
+			} else if(pos != -1 && type == 2){
 				hashtableAbb[pos]->printAktie();
-			} else {
+			} else{
 				std::cout << "Aktie kann nicht gefunden werden" << std::endl;
 			}
-		} else if (userinput == 3) {
+		} else if(userinput == 3){
 			string kurzel;
 			cout << "Geben sie das Kurzel der Aktie an deren Werte Sie importieren mochten" << endl;
 			cin >> kurzel;
 			int pos = searchPos(kurzel, hashtableAbb, 2);
 			importData(*hashtableAbb[pos]);
 			//hashtableAbb[pos]->printVector(high);
-		} else if (userinput == 4) {
+		} else if(userinput == 4){
 			string kurzel;
 			cout << "Geben sie das Kurzel der Aktie an die Sie anzeigen mochten" << endl;
 			cin >> kurzel;
@@ -366,9 +415,11 @@ int main() {
 			deleteAktie(delname);
 		} else if(userinput == 6){
 			saveToFile();
-		} else if (userinput == 0) {
+		} else if(userinput == 7){
+			loadDatabase();
+		} else if(userinput == 0){
 			break;
-		} else {
+		} else{
 			std::cout << "Bitte geben Sie eine der angef�hrten Nummern ein" << std::endl;
 		}
 	}
